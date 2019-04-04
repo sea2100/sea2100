2a57cf47a09ad0fa7cfcdc00483260172a09330a
Svetlana <Sea2100@mail.ru>
2019-04-04 08:26:18 +0300

    1 прога

---
+++ Task1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 16:35:24 2019
+@author: s.suvorova
+"""
+
+numb=int(input('Введите число '))
+print('к вашему числу прибавили 2 и получили',numb+2)
+
